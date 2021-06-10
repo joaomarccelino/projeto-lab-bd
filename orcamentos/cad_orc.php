56b597a1e79f6b42a6376a7f85503a8ac3e2167c
Jordana Momberg <jordana.momberg@meritt.com.br>
2021-06-09 22:24:28 -0300

    feat:implements layout login

--- orcamentos/cad_orc.php
+++ orcamentos/cad_orc.php
@@ -19,7 +19,7 @@ if (session_status() !== PHP_SESSION_ACTIVE) {
         serviços</title>
 </head>
 
-<body>
+<body style="background-color: #fafafa;">
     <?php
     if (isset($_SESSION['msg'])) {
         echo $_SESSION['msg'];
@@ -130,7 +130,6 @@ if (session_status() !== PHP_SESSION_ACTIVE) {
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-</body>
 
 </body>
 
