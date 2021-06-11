3711170f5a128260ecdae0138786bd0f194c0fb7
Jordana Momberg <jordana.momberg@meritt.com.br>
2021-06-10 22:35:04 -0300

    feat: message success and error

--- categoria/inc_cat.php
+++ categoria/inc_cat.php
@@ -4,9 +4,12 @@ include('../connection.php');
 
 $query = "INSERT INTO categoria (categoria) VALUES ('$categoria')";
 $resu = mysqli_query($con, $query);
-if (mysqli_insert_id($con)){
-    print "<br> Inclusão realizada com sucesso";
+if (mysqli_insert_id($con)) {
+    print "<div style='color: #32cd32; display: flex; align-items:center; margin-bottom: 12px'>
+    <img src='/projeto-lab-bd/assets/icons/check.svg' alt='check' style='width: 22px; margin-right: 8px'/> 
+    Inclusão realizada com sucesso
+    </div>";
+} else {
+    print "<div style='color: #d32f2f> Ops! Algo deu errado </div>";
 }
 mysqli_close($con);
-
-?>
