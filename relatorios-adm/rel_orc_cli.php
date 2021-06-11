d30541d6cb2a98a6a32ca1b4f10d263f92d390a0
joaomarccelino <joao.marcelino.esc@gmail.com>
2021-06-11 18:01:15 -0300

    Relatórios quase prontos

---
+++ relatorios-adm/rel_orc_cli.php
@@ -0,0 +1,53 @@
+<?php
+    include('../connection.php')
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="icon" href="/projeto-lab-bd/assets/favicon.png" type="image/x-icon" />
+    <title>Relatório de Orçamentos</title>
+</head>
+<body>
+    <h1>Pesquisar Orçamentos</h1>
+    <form action="" method="post">
+        <label>Nome do Cliente: </label>
+        <input type="text" name="nome_cliente">
+        <input type="submit" name="searchService" value="Pesquisar">
+    </form>
+    <br>
+    <div style="display: flex;align-items: center;justify-content: space-between">
+            <a href="../main.html">← Voltar</a>
+        </div>
+    <table border="1" width="100%" class="table  table-striped" style="margin: 40px 0 60px 0; ">
+        <thead>
+            <tr>
+                <th>Nº</th>
+                <th>Data de Início</th>
+                <th>Valor</th>
+                <th>Data de Expiração</th>
+                <th>Cliente</th>
+                <th>Serviço</th>
+                <th>Prestador</th>
+                <th>Observação</th>
+            </tr>
+    </thead>
+    <?php
+    $searchService = filter_input(INPUT_POST, 'searchService', FILTER_SANITIZE_STRING);
+    if ($searchService) {
+        $nome_cliente = filter_input(INPUT_POST, 'nome_cliente', FILTER_SANITIZE_STRING);
+        $query = "SELECT * FROM orcamento_cliente_prestador WHERE nome_cliente LIKE '%$nome_cliente%'  LIMIT 5"; 
+        $result = mysqli_query($con, $query);
+        while($orc = mysqli_fetch_assoc($result)) {
+            echo "<tr><th>" . $orc['id_orcamento'] . "</th><th> ";
+                echo $orc['data_inicio'] . "</th><th>"  . $orc['valor'] . "</th><td>" . $orc['data_expiracao'] . "</td><td>";
+                echo $orc['nome_cliente'] . "</td><td>" . $orc['servico'] . "</td><td>" . $orc['nome'] . "</td><td>";
+                echo $orc['observacao'] . "</td>";
+        }       
+    }
+    ?>
+    </table>
+</body>
+</html>
---
+++ relatorios-cli/rel_serv.php
@@ -0,0 +1,46 @@
+<?php
+    include('../connection.php')
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="icon" href="/projeto-lab-bd/assets/favicon.png" type="image/x-icon" />
+    <title>Relatório de Serviços</title>
+</head>
+<body>
+    <h1>Pesquisar Serviços</h1>
+    <form action="" method="post">
+        <label>Serviço: </label>
+        <input type="text" name="servico">
+        <input type="submit" name="searchService" value="Pesquisar">
+    </form>
+    <br>
+    <div style="display: flex;align-items: center;justify-content: space-between">
+            <a href="../main.html">← Voltar</a>
+        </div>
+    <table border="1" width="100%" class="table  table-striped" style="margin: 40px 0 60px 0; ">
+            <thead>
+                <tr>
+                    <th>Nº</th>
+                    <th>Serviço</th>
+                    <th>Categoria</th>
+                </tr>
+            </thead>
+    <?php
+    $searchService = filter_input(INPUT_POST, 'searchService', FILTER_SANITIZE_STRING);
+    if ($searchService) {
+        $servico = filter_input(INPUT_POST, 'servico', FILTER_SANITIZE_STRING);
+        $query = "SELECT * FROM servico_categoria WHERE servico LIKE '%$servico%' LIMIT 5"; 
+        $result = mysqli_query($con, $query);
+        while($serv = mysqli_fetch_assoc($result)) {
+            echo "<tr><td>" . $serv['id_servico'] . "</td><td> ";
+            echo $serv['servico'] . "</td><td>" . $serv['servico'] . "</td>";
+        }       
+    }
+    ?>
+    </table>
+</body>
+</html>
